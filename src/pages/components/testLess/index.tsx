cb6d8ed174db4453d1ef8fddd16b2b900d015c22
yiran_mac18 <studyyiran@163.com>
2020-05-23 01:01:21 +0800

    安装了prettier

--- src/pages/components/testLess/index.tsx
+++ src/pages/components/testLess/index.tsx
@@ -1,15 +1,18 @@
 import React, { useContext, useEffect } from "react";
 import "./index.less";
 
-interface ITestName {
+interface ITestName {}
 
-}
-
-export const TestLess: React.FC<ITestName> = props => {
-  return <div className="test-less">
-    <ul>
-      <li>123</li>
-      <li>456</li>
-    </ul>
-  </div>;
-}
+export const TestLess: React.FC<ITestName> = (props) => {
+  useEffect(() => {
+    console.log(props);
+  }, []);
+  return (
+    <div className="test-less">
+      <ul>
+        <li>123</li>
+        <li>456</li>
+      </ul>
+    </div>
+  );
+};
