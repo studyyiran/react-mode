2961e653f6a5aa21c579eb6d133280abd5a10ed2
yiranzi <570932296@qq.com>
2020-05-29 12:06:57 +0800

    完成buildserver

--- config/webpackserver.config.js
+++ config/webpackserver.config.js
@@ -168,9 +168,7 @@ module.exports = function(webpackEnv) {
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
-      filename: isEnvProduction
-        ? 'static/serverjs/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/serverjs/bundle.js',
+      filename: "static/serverjs/bundle.js",
       // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
