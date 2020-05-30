b1c0356cae0995b50cc72f8c98c9603606ab4641
yiran_mac18 <studyyiran@163.com>
2020-05-30 11:04:32 +0800

    准备开ssr

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 git pull
-npm install
-npm run buildserver
-npm run build
+yarn install
+yarn run buildserver
+yarn run build
 mv testbuild/server.js build/server.js
 pm2 delete server
 pm2 start build/server.js
